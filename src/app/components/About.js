502d03b289150b0233bfe23e63c92c1fb87ce629
Vivek Singh <vviveksingh8874@gmail.com>
2025-12-01 13:15:40 +0530

    about changed

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -29,13 +29,13 @@ export default function About() {
   ]
 
   return (
-    <section id="about" className="py-20 bg-gradient-to-b from-[#020617]/80 to-[#040B16]/90">
+    <section id="about" className="py-20 bg-linear-to-b from-[#020617]/80 to-[#040B16]/90">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
         {/* Header */}
         <h2 className="text-4xl md:text-5xl font-bold text-center mb-16 animate-fade-in">
           About{" "}
-          <span className="bg-gradient-to-r from-[#00A8FF] via-[#24C9FF] to-[#005DFF] bg-clip-text text-transparent">
+          <span className="bg-linear-to-r from-[#00A8FF] via-[#24C9FF] to-[#005DFF] bg-clip-text text-transparent">
             VORONIX
           </span>
         </h2>
@@ -48,7 +48,7 @@ export default function About() {
               className="group relative bg-slate-900/60 border border-slate-800 hover:border-blue-500/50 
               rounded-2xl p-8 transition-all duration-500 hover:shadow-[0_0_35px_#009DFF40]"
             >
-              <div className="absolute inset-0 bg-gradient-to-br from-[#00A8FF]/10 to-[#005DFF]/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded-2xl"></div>
+              <div className="absolute inset-0 bg-linear-to-br from-[#00A8FF]/10 to-[#005DFF]/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded-2xl"></div>
 
               <div className="relative z-10 text-center">
                 <item.icon className="w-12 h-12 mx-auto mb-4 text-blue-400 drop-shadow-[0_0_10px_#00A8FF80]" />
@@ -62,7 +62,7 @@ export default function About() {
         {/* Capabilities */}
         <h3 className="text-3xl font-bold text-center mb-10">
           What{" "}
-          <span className="bg-gradient-to-r from-[#00A8FF] to-[#005DFF] bg-clip-text text-transparent">
+          <span className="bg-linear-to-r from-[#00A8FF] to-[#005DFF] bg-clip-text text-transparent">
             We Do
           </span>
         </h3>
