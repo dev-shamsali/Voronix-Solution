1e049f3d6e49d367d0899785f9f953262ab78e28
Vivek Singh <vviveksingh8874@gmail.com>
2025-12-01 12:21:11 +0530

    hero section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 "use client"
-import Navbar from "./Navbar";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 
 export default function Home() {
   return (
     <>
       <Navbar/>
+      <Hero />
     </>
   );
 }
